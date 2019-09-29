259988114b247540aed0a963f3fbd2889763da00
Devon <miss.edwards.joseph@gmail.com>
2019-09-29 19:24:02 +0100

    Week3: Deleted class example, added own example of using Promise class

---
+++ promises_only.js
@@ -0,0 +1,29 @@
+function makeRequest(webpage) {
+    return new Promise((resolve, reject) => {
+        console.log(`Sending your request to: ${webpage}`)
+        if (webpage == 'Best Website Ever') {
+            resolve(`You have logged into: ${webpage}`)
+        }
+        else {
+            reject(`You picked a rubbish website. Try again.`)
+        }
+    })
+}
+
+
+function formatRequest(message) {
+    return new Promise((resolve, reject) => {
+        console.log(`About to format your request...`)
+        resolve(`Congratulations! Here's your message; ${message}`)
+    })
+}
+
+
+makeRequest('Best Website Ever').then(response => {
+    console.log(`Processing Request...`)
+    return formatRequest(response)
+}).then(formattedRequest => {
+    console.log(formattedRequest)
+}).catch(err => {
+    console.log(`Oops: ${err}`)
+})
